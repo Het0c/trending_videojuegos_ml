ca72bd2e65927449308d21ef938a57b1ec98214c
het0c <hlopez.htilg@gmail.com>
2025-10-23 18:47:51 +0000

    cambio tree

--- PROYECTO/trenging-videojuegos/src/trenging_videojuegos/__init__.py
+++
@@ -1,4 +0,0 @@
-"""trenging_videojuegos
-"""
-
-__version__ = "0.1"
---
+++ src/trenging_videojuegos/pipelines/gaming_analysis/nodes.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+def cargar_datos():
+    df = pd.read_csv("data/01_raw/gaming.csv", parse_dates=["Date"])
+    return df
+
+def procesar_datos(df):
+    df["dias"] = (df["Date"] - df["Date"].min()).dt.days
+    return df
+
+def calcular_estadisticas(df):
+    media = df["Revenue"].mean()
+    mediana = df["Revenue"].median()
+    return {"media": media, "mediana": mediana}
+
+
+
+def transformar_datos(df: pd.DataFrame) -> pd.DataFrame:
+    # Convertir fecha
+    df["Date"] = pd.to_datetime(df["Date"])
+    df["Year"] = df["Date"].dt.year
+    df["Month"] = df["Date"].dt.month
+    df["Weekday"] = df["Date"].dt.day_name()
+
+    # Crear nuevas features
+    df["Purchases_per_user"] = df["In-game Purchases ($)"] / df["Daily Active Users (DAU)"]
+    df["Engagement"] = df["Session Duration (minutes)"] * df["Daily Active Users (DAU)"]
+    df["Social_Impact"] = df["Social Media Mentions"] + df["Influencer Endorsements"]
+
+    # Codificar variables categóricas
+    df = pd.get_dummies(df, columns=["Platform", "Top Genre"], drop_first=True)
+
+    # Reordenar columnas (opcional)
+    columnas_principales = [
+        "Date", "Year", "Month", "Weekday", "Daily Active Users (DAU)",
+        "New Registrations", "Session Duration (minutes)", "In-game Purchases ($)",
+        "Revenue", "Purchases_per_user", "Engagement", "Social_Impact"
+    ]
+    columnas_finales = columnas_principales + [col for col in df.columns if col not in columnas_principales]
+    df = df[columnas_finales]
+
+    return df
